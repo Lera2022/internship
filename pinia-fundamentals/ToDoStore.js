a76ee9ebde0acea8a40aca5e195eb5bfc841265e
Lera <valeriiakolesnikova21@gmail.com>
2024-10-18 13:09:28 +0300

    add pinia

---
+++ pinia-fundamentals/ToDoStore.js
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+export const useTodoStore = defineStore('ToDoStore', {
+    state: () => ({
+        todos: []
+    }),
+    actions: {
+        addTodo(todo){
+            this.todos.push(todo)
+        }
+    },
+    getters: {
+        doneTodos: (state) => state.todos.filter(todo => todo.done)
+    }
+})
